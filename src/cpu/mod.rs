cc5fb58fb30966d948dd37ae062b940db11168b0
Luiz Schonarth <luizcschonarth@gmail.com>
2025-05-13 22:04:41 -0300

    Introduce CpuState struct

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,2 +1,24 @@
+use registers::{Flags, RawRegisters};
+
 mod instructions;
 mod registers;
+
+#[derive(Debug)]
+pub struct CpuState {
+    // TODO add memory field
+    pc: usize,
+    sp: usize,
+    flags: Flags,
+    regs: RawRegisters,
+}
+
+impl CpuState {
+    // fn fetch_instruction(&self) -> Instruction {
+    //     Instruction::from_bytes()
+    // }
+
+    pub fn step(&mut self) {
+        // let instr = self.fetch_instruction();
+        // instr.execute(self);
+    }
+}
